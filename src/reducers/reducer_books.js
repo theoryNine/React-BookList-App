f5d15f1bdc5c0e28c3905351e75faad80b526bf7
Ansel Colvin <anselcolvin@gmail.com>
2017-10-19 12:52:49 -0400

    Building reducers

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'JavaScript: The Good Parts' },
+        { title: 'The Dark Tower' },
+        { title: 'Star Trek' },
+        { title: 'Ring World' }
+    ];
+}
